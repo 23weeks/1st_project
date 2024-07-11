008c5b6adfea3a92ada465554769a6ea8d969359
상주 <110362435+23weeks@users.noreply.github.com>
2024-07-11 17:58:30 +0900

    commit

---
+++ demo/frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../css/base.css';
+
+function Footer() {
+	return (
+		<div className="footer">
+			<h2>생성중</h2>
+			<ul className="authList">
+				<li>상주&nbsp;<a href="https://github.com/23weeks" target="_blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" /></a></li>
+				<li>예중&nbsp;<a href="https://github.com/yehjungkim" target="_blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" /></a></li>
+			</ul>
+		</div>
+	);
+}
+
+export default Footer;
